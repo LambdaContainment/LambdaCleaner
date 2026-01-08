acbd907bad1a87d3d257bfbace8f91e501317384
WinG4merBR <68250074+WinG4merBR@users.noreply.github.com>
2026-01-08 16:23:21 -0300

    First commit!

---
+++ LambdaCleaner.cs
@@ -0,0 +1,43 @@
+﻿using Exiled.API.Features;
+using MEC;
+
+namespace LambdaCleaner;
+
+public class LambdaCleaner : Plugin<Config>
+{
+    public override string Name => "LambdaCleaner";
+    public override Version Version => new Version(1, 0, 0);
+    public PeriodicalCleanup cleaner;
+    public CoroutineHandle cleanupCoroutine;
+    
+    public override void OnEnabled()
+    {
+        cleaner = new PeriodicalCleanup(this);
+        Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+        Exiled.Events.Handlers.Server.RestartingRound += onRoundRestart;
+        base.OnEnabled();
+    }
+
+    private void onRoundRestart()
+    {
+        Timing.KillCoroutines(cleanupCoroutine);
+    }
+    private void OnRoundStarted()
+    {
+        try
+        {
+            cleanupCoroutine = Timing.RunCoroutine(cleaner.RunCleanupLoop(5f));
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Error while starting Cleaner coroutine: {e}");
+        }
+    }
+    public override void OnDisabled()
+    {
+        Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+        Exiled.Events.Handlers.Server.RestartingRound -= onRoundRestart;
+        Timing.KillCoroutines(cleanupCoroutine);
+        base.OnDisabled();
+    }
+}
---
+++ PeriodicalCleanup.cs
@@ -0,0 +1,51 @@
+﻿using Exiled.API.Features;
+using MEC;
+using Mirror;
+using UnityEngine;
+
+namespace LambdaCleaner;
+
+public class PeriodicalCleanup
+{
+    public readonly LambdaCleaner plugin;
+    public PeriodicalCleanup(LambdaCleaner plugin) => this.plugin = plugin;
+    
+    public IEnumerator<float> RunCleanupLoop(float interval)
+    {
+        while (true)
+        {
+            yield return Timing.WaitForSeconds(interval);
+            
+            var allRagdolls = Ragdoll.List.ToList();
+            
+            if (allRagdolls.Count > 0)
+            {
+                Map.Broadcast(
+                    8,
+                    "<size=35><b><color=#00FFFF>[</color>LambdaCleaner<color=#00FFFF>]</color></b> <color=#eeeeee>Limpeza de corpos em 15 segundos...</color></size>"
+                );
+
+                yield return Timing.WaitForSeconds(15f);
+
+                Log.Info($"[LambdaCleaner] Cleaned {allRagdolls.Count} ragdolls");
+                int count = 0;
+
+                foreach (Ragdoll ragdoll in allRagdolls)
+                {
+                    if (ragdoll != null && ragdoll.GameObject != null)
+                    {
+                        NetworkServer.Destroy(ragdoll.Base.gameObject);
+                        count++;
+
+                        yield return Timing.WaitForSeconds(0.1f);
+                    }
+                }
+
+                if (count > 0)
+                {
+                    Log.Info($"[LambdaCleaner] Sucesso: {count} ragdolls removidos.");
+                }
+            }
+        }
+    }
+}
